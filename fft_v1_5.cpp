ffb430d0c96e9b55ef861ae81c3d109cc399d319
caim-d <60481244+caim-d@users.noreply.github.com>
2021-05-22 11:52:51 -0400

    Add files via upload
    added some more timings

--- fft_v1_5.cpp
+++ fft_v1_5.cpp
@@ -6,7 +6,7 @@
 #include <fftw3.h>
 #include <algorithm> // for min
 
-// mpic++ -std=c++14 fft_v1_5.cpp -lfftw3   >?-lm
+// mpic++ -std=c++14 -O3 fft_v1_5.cpp -lfftw3
 // mpiexec -np 4 ./a.out
 
 using namespace std::complex_literals;
@@ -17,7 +17,8 @@ double get_error(std::complex<double> *fk, fftw_complex *out, int N);
 int my_log2(int N);
 int reverse_bits(int x, int s);
 int get_partner(int rank, int size, int j);
-void fft_mpi(std::complex<double> *fx, std::complex<double> *fk, int N, std::complex<double> *omega_, int rank, int size);
+void fft_mpi(std::complex<double> *fx, std::complex<double> *fk, int N, std::complex<double> *omega_, int rank, int size); //, double tt1);
+int my_pow2(int a);
 
 
 int main(int argc, char *argv[]) {
@@ -27,62 +28,79 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int N = 1024;
+    for (int a=7; a<21; a++) {
+		int N = my_pow2(a);
+        if (rank == 0) {
+	        printf("N = %d \n \n", N);
+	    }
 
-    std::complex<double> *omega_ = (std::complex<double> *) malloc(N* sizeof(std::complex<double>));
+	    std::complex<double> *omega_ = (std::complex<double> *) malloc(N* sizeof(std::complex<double>));
 
-    // array of exp(-1i*j*k*2pi/N)
-    for (int j=0; j<N; j++) {
-        omega_[j] = exp(-1i*(2.*M_PI*j/N));
+        // array of exp(-1i*j*k*2pi/N)
+        for (int j=0; j<N; j++) {
+            omega_[j] = exp(-1i*(2.*M_PI*j/N));
+        }
+
+        std::complex<double> *fx = (std::complex<double> *) malloc(N* sizeof(std::complex<double>));
+        std::complex<double> *fx_copy = (std::complex<double> *) malloc(N* sizeof(std::complex<double>));
+        std::complex<double> *fx_copy2 = (std::complex<double> *) malloc(N* sizeof(std::complex<double>));
+   	    std::complex<double> *fk = (std::complex<double> *) calloc(N, sizeof(std::complex<double>));
+        std::complex<double> *fk2 = (std::complex<double> *) calloc(N, sizeof(std::complex<double>));
+        std::complex<double> *fk3 = (std::complex<double> *) calloc(N, sizeof(std::complex<double>));
+
+        // initialize vector to take fft of
+        for (int j=0; j<N; j++) {
+            fx[j] = sin(2*j)+cos(3*j)*1i;
+            fx_copy[j] = fx[j];
+            fx_copy2[j] = fx[j];
+        }
+
+        // do MPI version
+        double tt1 = MPI_Wtime();
+	    fft_mpi(fx_copy2, fk3, N, omega_, rank, size); //, tt1);
+	    tt1 = MPI_Wtime() - tt1;
+
+        // do fftw version, page 3 of http://www.fftw.org/fftw3.pdf
+        fftw_complex *in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+    	fftw_complex *out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);;
+    	fftw_plan p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);;
+
+    	for (int j=0; j<N; j++) {
+            in[j][0] = sin(2*j);
+            in[j][1] = cos(3*j);
+    	}
+
+    	if (rank == 0) {
+            double tt3 = MPI_Wtime();
+	        fft_serial(fx_copy, fk2, N, omega_);
+	        tt3 = MPI_Wtime() - tt3;
+
+	        double tt2 = MPI_Wtime();
+            fftw_execute(p);
+	        tt2 = MPI_Wtime() - tt2;
+
+            printf("time for fftw = %g \n", tt2);
+
+            printf("error for fft_serial = %f \n", get_error(fk2,out,N));
+            printf("time for fft_serial = %g \n", tt3);
+
+            printf("error for fft_mpi = %f \n", get_error(fk3,out,N));
+            printf("time for fft_mpi = %g \n \n", tt1);
+
+	        fftw_destroy_plan(p);
+	        fftw_free(in);
+	        fftw_free(out);
+	        free(omega_);
+	        free(fx);
+	        free(fx_copy);
+	        free(fx_copy2);
+    	    free(fk);
+	        free(fk2);
+	        free(fk3);
+        }
+
+	MPI_Barrier(MPI_COMM_WORLD);
     }
-
-    std::complex<double> *fx = (std::complex<double> *) malloc(N* sizeof(std::complex<double>));
-    std::complex<double> *fx_copy = (std::complex<double> *) malloc(N* sizeof(std::complex<double>));
-    std::complex<double> *fx_copy2 = (std::complex<double> *) malloc(N* sizeof(std::complex<double>));
-    std::complex<double> *fk = (std::complex<double> *) calloc(N, sizeof(std::complex<double>));
-    std::complex<double> *fk2 = (std::complex<double> *) calloc(N, sizeof(std::complex<double>));
-    std::complex<double> *fk3 = (std::complex<double> *) calloc(N, sizeof(std::complex<double>));
-
-    // initialize vector to take fft of
-    for (int j=0; j<N; j++) {
-        fx[j] = sin(2*j)+cos(3*j)*1i;
-        fx_copy[j] = fx[j];
-        fx_copy2[j] = fx[j];
-    }
-
-    // do MPI version
-    fft_mpi(fx_copy2, fk3, N, omega_, rank, size);
-
-    // do fftw version, page 3 of http://www.fftw.org/fftw3.pdf
-    fftw_complex *in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
-    fftw_complex *out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);;
-    fftw_plan p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);;
-
-    for (int j=0; j<N; j++) {
-        in[j][0] = sin(2*j);
-        in[j][1] = cos(3*j);
-    }
-
-    if (rank == 0) {
-        dft_naive(fx, fk, N);
-        fft_serial(fx_copy, fk2, N, omega_);
-        fftw_execute(p);
-
-        printf("error for dft_naive = %f \n", get_error(fk,out,N));
-        printf("error for fft_serial = %f \n", get_error(fk2,out,N));
-        printf("error for fft_mpi = %f \n", get_error(fk3,out,N));
-    }
-
-    fftw_destroy_plan(p);
-    fftw_free(in);
-    fftw_free(out);
-    free(omega_);
-    free(fx);
-    free(fx_copy);
-    free(fx_copy2);
-    free(fk);
-    free(fk2);
-    free(fk3);
 
     MPI_Finalize();
     return 0;
@@ -124,6 +142,17 @@ int my_log2(int N) {
 }
 
 //------------------------------------------------------------------------------
+
+int my_pow2(int a) {
+    int e{2}, a_temp{a};
+    while (a_temp > 1) {
+	a_temp -= 1;
+	e *= 2;
+    }
+    return e;
+}
+
+//-------------------------------------------------------------------------------
 
 int reverse_bits(int x, int s) {
     int x_reverse{}, temp;
@@ -178,7 +207,7 @@ void fft_serial(std::complex<double> *fx, std::complex<double> *fk, int N, std::
 
 //------------------------------------------------------------------------------
 
-void fft_mpi(std::complex<double> *fx, std::complex<double> *fk, int N, std::complex<double> *omega_, int rank, int size) {
+void fft_mpi(std::complex<double> *fx, std::complex<double> *fk, int N, std::complex<double> *omega_, int rank, int size) { //, double tt1) {
     MPI_Status status;
     int s = my_log2(N), p = my_log2(size);
     int mask, index0, index1, partner, k_global, Nlocal = N/size;
@@ -245,4 +274,5 @@ void fft_mpi(std::complex<double> *fx, std::complex<double> *fk, int N, std::com
             fk[k] = fx[reverse_bits(k,s)];
         }
     }
+//    tt1 = MPI_Wtime() - tt1;
 }
